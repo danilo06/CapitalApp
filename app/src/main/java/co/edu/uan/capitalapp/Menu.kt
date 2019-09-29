656e500b8c8d7642ebe54a890e82f4779a727324
Danilo Beleño <dbeleno86@uan.edu.co>
2019-09-28 19:26:11 -0500

    Chage to scene

--- app/src/main/java/co/edu/uan/capitalapp/Menu.kt
+++ app/src/main/java/co/edu/uan/capitalapp/Menu.kt
@@ -1,12 +1,20 @@
 package co.edu.uan.capitalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class Menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+    }
+
+    fun clickButton(view: View){
+            val intent = Intent(this, co.edu.uan.capitalapp.AgendaDeGastos::class.java)
+            startActivity(intent)
     }
 }
