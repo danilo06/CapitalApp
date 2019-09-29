389d5b170c6ea1a99b8e1dba7766568b39c9db5a
Danilo Beleño <dbeleno86@uan.edu.co>
2019-09-28 19:09:42 -0500

    Cambios vista generada segun modelo

---
+++ app/src/main/java/co/edu/uan/capitalapp/AgendaDeGastos.kt
@@ -0,0 +1,12 @@
+package co.edu.uan.capitalapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendaDeGastos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda_de_gastos)
+    }
+}
