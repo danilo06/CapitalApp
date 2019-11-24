f36d8a0e8f682b05083e3dba4b160b1426c3fc1a
Danilo Beleño <dbeleno86@uan.edu.co>
2019-11-24 14:22:29 -0500

    Vista Nomina Agregada y correccion de calculo horas extra

--- app/src/main/java/co/edu/uan/capitalapp/HorasExtra.kt
+++ app/src/main/java/co/edu/uan/capitalapp/HorasExtra.kt
@@ -2,6 +2,7 @@ package co.edu.uan.capitalapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.horasextra.*
@@ -13,26 +14,53 @@ class HorasExtra : AppCompatActivity() {
         setContentView(R.layout.horasextra)
     }
 
-    fun calcular(view: View){
-        if(editTextSalarioBase.text.toString().isNullOrEmpty() || editTextDiurna.text.toString().isNullOrEmpty() || editTextNocturna.text.toString().isNullOrEmpty() || editTextFestiva.text.toString().isNullOrEmpty()|| editTextFestivaDiurna.text.toString().isNullOrEmpty() || editTextFestivaNocturna.text.toString().isNullOrEmpty()){
-            Toast.makeText(applicationContext,"Campos sin llenar, por favor termiar", Toast.LENGTH_SHORT).show()
-        }else
-        {
-            val salario = editTextSalarioBase.text.toString().toDouble()
-            val diurna = editTextDiurna.text.toString().toDouble()
-            val nocturna = editTextNocturna.text.toString().toDouble()
-            val festiva = editTextFestiva.text.toString().toDouble()
-            val diurnafestiva = editTextFestivaDiurna.text.toString().toDouble()
-            val nocturnafestiva = editTextFestivaNocturna.text.toString().toDouble()
-            val valorHora = salario/240
-            var total = 0.0
-            total += (valorHora*1.25) * diurna
-            total += (valorHora*1.75) * nocturna
-            total += (valorHora*1.75) * festiva
-            total += (valorHora*2) * diurnafestiva
-            total += (valorHora*2.5) * nocturnafestiva
-            editTextTotal.setText("   $ "+total.toString())
+    fun Run(view: View) {
+        if (editTextSalarioBase.text.toString().isNullOrEmpty()) {
+            Toast.makeText(
+                applicationContext,
+                "Salario sin llenar, por favor asignar",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            validaContenido(editTextDiurna)
+            validaContenido(editTextNocturna)
+            validaContenido(editTextFestiva)
+            validaContenido(editTextFestivaDiurna)
+            validaContenido(editTextFestivaNocturna)
+            editTextTotal.setText(
+                "   $ " + calcula(
+                    editTextSalarioBase.text.toString().toDouble(),
+                    editTextDiurna.text.toString().toDouble(),
+                    editTextNocturna.text.toString().toDouble(),
+                    editTextFestiva.text.toString().toDouble(),
+                    editTextFestivaDiurna.text.toString().toDouble(),
+                    editTextFestivaNocturna.text.toString().toDouble()
+                )
+            )
         }
-
     }
+
+    private fun validaContenido(param1: EditText) {
+        if (param1.text.toString().isNullOrEmpty())
+            param1.setText("0")
+    }
+
+    fun calcula(
+        salario: Double,
+        diurna: Double,
+        nocturna: Double,
+        festiva: Double,
+        diurnaFestiva: Double,
+        nocturnaFestiva: Double
+    ): Double {
+        val valorHora = salario / 240
+        var total = 0.0
+        total += (valorHora * 1.25) * diurna
+        total += (valorHora * 1.75) * nocturna
+        total += (valorHora * 1.75) * festiva
+        total += (valorHora * 2) * diurnaFestiva
+        total += (valorHora * 2.5) * nocturnaFestiva
+        return total
+    }
+
 }
